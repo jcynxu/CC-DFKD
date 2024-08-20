7202e1157f1b01d20f3bb3cbc0222310127dd4f1
jiangchengyang414122 <1075853634@qq.com>
2024-08-20 14:58:12 +0800

    scripts/

---
+++ scripts/CC-DFKD/cifar100_wrn40_2_wrn16_2.sh
@@ -0,0 +1,21 @@
+python datafree_kd.py \
+--method CC-DFKD \
+--dataset cifar100 \
+--batch_size 256 \
+--synthesis_batch_size 256 \
+--teacher wrn40_2 \
+--student wrn16_2 \
+--lr 0.08 \
+--kd_steps 400 \
+--ep_steps 400 \
+--g_steps 200 \
+--lr_g 2e-3 \
+--gpu 1 \
+--seed 0 \
+--T 20 \
+--save_dir run/sample_gapssg \
+--log_tag log_gapssg \
+--co_alpha 5 \
+--co_beta 1 \
+--co_gamma 0.7 \
+--co_eta 0.7
